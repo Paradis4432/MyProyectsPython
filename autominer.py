97574bf439e19baaac901f34ca5dd4bdb5554cf8
Paradis4432 <49301263+Paradis4432@users.noreply.github.com>
2021-05-29 15:18:40 -0300

    Add files via upload

---
+++ autominer.py
@@ -0,0 +1,66 @@
+import pydirectinput
+import pyautogui
+import time
+
+
+def moveRight():
+    pyautogui.move(-300, 0)
+    pyautogui.move(-300, 0)
+    pyautogui.move(-300, 0)
+    pyautogui.move(-300, 0)
+    pyautogui.move(-250, 0)
+
+
+def holdMouse():
+    pydirectinput.mouseDown()
+
+
+def pressQ():
+    pydirectinput.keyDown('q')
+    pydirectinput.keyUp('q')
+
+
+def moveUp():
+    pydirectinput.keyDown('space')
+    time.sleep(2)
+    pydirectinput.keyUp('space')
+
+
+def moveDown():
+    pydirectinput.keyDown('shift')
+    time.sleep(4)
+    pydirectinput.keyUp('shift')
+
+
+def pressW():
+    pydirectinput.keyDown('w')
+
+
+def pressD():
+    pydirectinput.keyDown('d')
+
+
+time.sleep(2)  # so u can switch to your game
+
+pressD()
+holdMouse()
+cont = 0
+laps = 0
+
+while True:
+    holdMouse()
+    time.sleep(9)
+    pressQ()
+    time.sleep(9)
+    moveRight()
+    cont = cont + 1
+    print(cont, "cont plus")
+    if cont == 3:
+        cont = 0
+        if laps == 2:
+            laps = 0
+            moveDown()
+            continue
+        moveUp()
+        laps = laps + 1
+        print(laps, "laps plus")
